b81f2a33affecfa9a2aee11f8d976da03827fd82
Christopher Santos <112826117+ochristopherfilipe@users.noreply.github.com>
2023-08-14 15:51:12 -0400

    teste com streamlit

---
+++ Teste_Streamlit.py
@@ -0,0 +1,341 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+import altair as alt
+import plotly.figure_factory as ff
+import plotly.express as px
+import pydeck as pdk
+
+st.set_page_config(layout='wide')
+
+st.title('Viagens de Uber em Nova Iorque')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
+
+
+
+
+st.subheader('Carregando algo aleatório:')
+
+'Começando um longo processo, aguarde!...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...Prontinho, pode continuar!'
+
+st.subheader('Mapa de Manaus')
+
+
+df = pd.DataFrame(
+    np.random.randn(500, 2) / [70, 70] + [-3.09, -60.025780],
+    columns=['lat', 'lon'])
+
+st.map(df)
+
+
+st.subheader('DataFrame personalizado')
+
+
+df = pd.DataFrame(
+    {
+        "name": ["Roadmap", "Extras", "Issues"],
+        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+        "stars": [np.random.randint(0, 1000) for _ in range(3)],
+        "views_history": [[np.random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+    }
+)
+st.dataframe(
+    df,
+    width= 1058,
+    column_config={
+        "name": "App name",
+        "stars": st.column_config.NumberColumn(
+            "Github Stars",
+            help="Number of stars on GitHub",
+            format="%d ⭐",
+        ),
+        "url": st.column_config.LinkColumn("App URL"),
+        "views_history": st.column_config.LineChartColumn(
+            "Views (past 30 days)", y_min=0, y_max=5000
+        ),
+    },
+    hide_index=True,
+)
+
+
+st.subheader('BarChart')
+
+
+data_df = pd.DataFrame(
+    {
+        "sales": [
+            [0, 4, 26, 80, 100, 40],
+            [80, 20, 80, 35, 40, 100],
+            [10, 20, 80, 80, 70, 0],
+            [10, 100, 20, 100, 30, 100],
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.BarChartColumn(
+            "Sales (last 6 months)",
+            help="The sales volume in the last 6 months",
+            y_min=0,
+            y_max=100,
+        ),
+    },
+    hide_index=True,
+)
+
+
+st.subheader('ProgressColumn')
+
+data_df = pd.DataFrame(
+    {
+        "sales": [200, 550, 1000, 80],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.ProgressColumn(
+            "Sales volume",
+            help="The sales volume in USD",
+            format="$%f",
+            min_value=0,
+            max_value=1000,
+        ),
+    },
+    hide_index=True,
+)
+
+st.subheader('Métricas')
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
+st.subheader('Select Box')
+
+data_df = pd.DataFrame(
+    {
+        "category": [
+            "📊 Data Exploration",
+            "📈 Data Visualization",
+            "🤖 LLM",
+            "📊 Data Exploration",
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "category": st.column_config.SelectboxColumn(
+            "App Category",
+            help="The category of the app",
+            width="medium",
+            options=[
+                "📊 Data Exploration",
+                "📈 Data Visualization",
+                "🤖 LLM",
+            ],
+        )
+    },
+    hide_index=True,
+)
+
+
+st.subheader('Line Chart')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+st.subheader('Area Chart')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
+
+st.subheader('Bar Chart')
+
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+
+st.subheader('Pyplot')
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
+st.subheader('Altair Chart')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+c = alt.Chart(chart_data).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.altair_chart(c, use_container_width=True)
+
+
+st.subheader('Plotly Chart')
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+
+df = px.data.gapminder()
+
+fig = px.scatter(
+    df.query("year==2007"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+    
+st.subheader("Define a custom colorscale")
+df = px.data.iris()
+fig = px.scatter(
+    df,
+    x="sepal_width",
+    y="sepal_length",
+    color="sepal_length",
+    color_continuous_scale="reds",
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+st.subheader('Pydeck Chart')
+    
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
+
+st.markdown('# Só relaxe e olhe o gatinho feliz')
+
+st.markdown(
+    '<img src="https://th.bing.com/th/id/OIP.D8ojwi7h1p4V0_3q6v71wgHaE8?w=296&h=197&c=7&r=0&o=5&pid=1.7" height="333" style="border: 5px solid orange">',
+    unsafe_allow_html=True,
+)
+
+with st.spinner('Wait for it...'):
+    time.sleep(5)
+st.success('Done!')
